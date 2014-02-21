880cc9d807d54dcea319a0d2c2c22772e9c08596
Nikolay Kasyanov <nikolay.kasyanov@softwearfinance.com>
2014-02-21 17:21:37 +0400

    Boost no longer builds single-threaded and static libraries, updated bottle

--- openmw-boost.rb
+++ openmw-boost.rb
@@ -1,17 +1,5 @@
 require 'formula'
 
-class UniversalPython < Requirement
-  satisfy(:build_env => false) { archs_for_command("python").universal? }
-
-  def message; <<-EOS.undent
-    A universal build was requested, but Python is not a universal build
-
-    Boost compiles against the Python it finds in the path; if this Python
-    is not a universal build then linking will likely fail.
-    EOS
-  end
-end
-
 class OpenmwBoost < Formula
   homepage 'http://www.boost.org'
   url 'http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz/download'
@@ -22,35 +10,20 @@ class OpenmwBoost < Formula
   bottle do
     root_url 'https://dl.dropboxusercontent.com/u/28481/openmw/bottles'
     cellar :any
-    revision 1
-    sha1 "89d3db74c9f8ce4d4dedce8fec3efbee3cc11769" => :mavericks
+    revision 2
+    sha1 "5b6e3bd12b15d6124c7dad57e8ca8c221a027b03" => :mavericks
   end
 
   env :userpaths
 
-  option :universal
   option 'with-icu', 'Build regexp engine with icu support'
-  option 'without-single', 'Disable building single-threading variant'
-  option 'without-static', 'Disable building static library variant'
-  option 'with-mpi', 'Build with MPI support'
   option :cxx11
-
-  depends_on :python => :recommended
-  depends_on UniversalPython if build.universal? and build.with? "python"
 
   if build.with? 'icu'
     if build.cxx11?
       depends_on 'icu4c' => 'c++11'
     else
       depends_on 'icu4c'
-    end
-  end
-
-  if build.with? 'mpi'
-    if build.cxx11?
-      depends_on 'open-mpi' => 'c++11'
-    else
-      depends_on :mpi => [:cc, :cxx, :optional]
     end
   end
 
@@ -61,24 +34,11 @@ class OpenmwBoost < Formula
     cause "Dropped arguments to functions when linking with boost"
   end
 
+  if Hardware::CPU.type == :ppc || Hardware::CPU.is_32_bit?
+    odie 'Only x86_64 builds are supported'
+  end
+
   def install
-    # https://svn.boost.org/trac/boost/ticket/8841
-    if build.with? 'mpi' and not build.without? 'single'
-      raise <<-EOS.undent
-        Building MPI support for both single and multi-threaded flavors
-        is not supported.  Please use '--with-mpi' together with
-        '--without-single'.
-      EOS
-    end
-
-    if build.cxx11? and build.with? 'mpi' and python
-      raise <<-EOS.undent
-        Building MPI support for Python using C++11 mode results in
-        failure and hence disabled.  Please don't use this combination
-        of options.
-      EOS
-    end
-
     ENV.universal_binary if build.universal?
     ENV.cxx11 if build.cxx11?
 
@@ -107,7 +67,6 @@ class OpenmwBoost < Formula
     # Force boost to compile using the appropriate GCC version
     open("user-config.jam", "a") do |file|
       file.write "using darwin : : #{ENV.cxx} : <compileflags>-mmacosx-version-min=10.6 <linkflags>-mmacosx-version-min=10.6 ;\n"
-      file.write "using mpi ;\n" if build.with? 'mpi'
     end
 
     # we specify libdir too because the script is apparently broken
@@ -123,21 +82,12 @@ class OpenmwBoost < Formula
     # Handle libraries that will not be built.
     without_libraries = []
 
-    # The context library is implemented as x86_64 ASM, so it
-    # won't build on PPC or 32-bit builds
-    # see https://github.com/mxcl/homebrew/issues/17646
-    if Hardware::CPU.type == :ppc || Hardware::CPU.is_32_bit? || build.universal?
-      without_libraries << "context"
-      # The coroutine library depends on the context library.
-      without_libraries << "coroutine"
-    end
-
     # Boost.Log cannot be built using Apple GCC at the moment. Disabled
     # on such systems.
     without_libraries << "log" if ENV.compiler == :gcc || ENV.compiler == :llvm
 
-    without_libraries << "python" if build.without? 'python'
-    without_libraries << "mpi" if build.without? 'mpi'
+    without_libraries << "python"
+    without_libraries << "mpi"
 
     bargs << "--without-libraries=#{without_libraries.join(',')}"
 
@@ -149,19 +99,8 @@ class OpenmwBoost < Formula
             "--user-config=user-config.jam",
             "install"]
 
-    if build.include? 'without-single'
-      args << "threading=multi"
-    else
-      args << "threading=multi,single"
-    end
-
-    if build.include? 'without-static'
-      args << "link=shared"
-    else
-      args << "link=shared,static"
-    end
-
-    args << "address-model=32_64" << "architecture=x86" << "pch=off" if build.universal?
+    args << "threading=multi"
+    args << "link=shared"
 
     system "./bootstrap.sh", *bargs
     system "./b2", *args
@@ -183,15 +122,6 @@ class OpenmwBoost < Formula
 
       Building of Boost.Context and Boost.Coroutine is disabled as they are
       only supported on x86_64.
-      EOS
-    end
-
-    if pour_bottle? and Formula.factory('python').installed?
-      s += <<-EOS.undent
-
-      The Boost bottle's module will not import into a Homebrew-installed Python.
-      If you use the Boost Python module then please:
-        brew install boost --build-from-source
       EOS
     end
     s
