23676e821cd4fc1b4e9c8a2b9964dc4b3ccf1975
Nikolay Kasyanov <corrmage@gmail.com>
2014-03-23 20:07:57 +0400

    #2 in progress: added c++11 option to Ogre formula, bumped deployment target, bottle now contains c++11 build

--- openmw-ogre19.rb
+++ openmw-ogre19.rb
@@ -8,16 +8,30 @@ class OpenmwOgre19 < Formula
     version '1.9.0'
 
     bottle do
-        root_url 'https://dl.dropboxusercontent.com/u/28481/openmw/bottles'
-        sha1 "40965abbd9e81d4ccb5cdc091c8a0d37eecdd08b" => :mavericks
+      root_url 'https://dl.dropboxusercontent.com/u/28481/openmw/bottles'
+      revision 1
+      sha1 "2ca7e0ddf854eae35b76113bb140f6c8fdffeff5" => :mavericks
     end
 
-    depends_on 'cmake' => :build
-    depends_on 'openmw-boost'
+    option :cxx11
 
-    resource 'dependencies' do
+    depends_on 'cmake' => :build
+    if build.cxx11?
+      depends_on 'openmw-boost' => 'c++11'
+    else
+      depends_on 'openmw-boost'
+    end
+
+    if build.cxx11?
+      resource 'dependencies' do
+        url 'http://sourceforge.net/projects/ogre/files/ogre-dependencies-mac/1.9/OgreDependencies_OSX_libc%2B%2B_20130610.zip/download'
+        sha1 'a9cacb347b22cfdca82e8329771a7a0b27ca495c'
+      end
+    else
+      resource 'dependencies' do
         url 'https://sourceforge.net/projects/ogre/files/ogre-dependencies-mac/1.8/OgreDependencies_OSX_20120525.zip/download'
         sha1 '75f173994b25a22eeb3b782cdf17222b336b44f6'
+      end
     end
 
     def patches
@@ -29,6 +43,10 @@ class OpenmwOgre19 < Formula
         resource('dependencies').stage do
             system "rm", "-rf", "Dependencies/include/boost"
             system "rm", "-rf", "Dependencies/include/OIS"
+            system "rm", "-rf", "Dependencies/lib/libboost_chrono.a"
+            system "rm", "-rf", "Dependencies/lib/libboost_date_time.a"
+            system "rm", "-rf", "Dependencies/lib/libboost_system.a"
+            system "rm", "-rf", "Dependencies/lib/libboost_thread.a"
 
             buildpath.install Dir['Dependencies']
         end
@@ -41,9 +59,9 @@ class OpenmwOgre19 < Formula
         args << "-DCMAKE_BUILD_TYPE=Release"
         args << "-DCMAKE_C_COMPILER=#{ENV.cc}"
         args << "-DCMAKE_CXX_COMPILER=#{ENV.cxx}"
-        args << "-DCMAKE_C_FLAGS=-mmacosx-version-min=10.6"
-        args << "-DCMAKE_CXX_FLAGS=-mmacosx-version-min=10.6"
-        args << "-DCMAKE_SHARED_LINKER_FLAGS=-mmacosx-version-min=10.6"
+        args << "-DCMAKE_OSX_ARCHITECTURES=x86_64"
+        args << "-DOGRE_CONFIG_ENABLE_LIBCPP_SUPPORT=TRUE" if build.cxx11?
+        args << "-DCMAKE_CXX_FLAGS='-stdlib=libc++ -std=c++11'" if build.cxx11?
 
         system "cmake", *args
         system "make"
@@ -80,3 +98,21 @@ diff --git a/CMake/Utils/OgreConfigTargets.cmake b/CMake/Utils/OgreConfigTargets
        )
        set_target_properties(${LIBNAME} PROPERTIES PUBLIC_HEADER "${HEADER_FILES} ${PLATFORM_HEADERS}")
        set_target_properties(${LIBNAME} PROPERTIES XCODE_ATTRIBUTE_GCC_PRECOMPILE_PREFIX_HEADER "YES")
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -39,8 +39,13 @@
+   SET(CMAKE_SIZEOF_VOID_P 4)
+   set(CMAKE_XCODE_ATTRIBUTE_GCC_VERSION "com.apple.compilers.llvm.clang.1_0")
+   if(OGRE_CONFIG_ENABLE_LIBCPP_SUPPORT)
+-    set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++11")
+-    set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++")
++    if (CMAKE_GENERATOR STREQUAL "Xcode")
++      set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++11")
++      set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++")
++    else ()
++      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -std=c++11")
++      set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -stdlib=libc++")
++    endif ()
+   endif()
+ endif ()
